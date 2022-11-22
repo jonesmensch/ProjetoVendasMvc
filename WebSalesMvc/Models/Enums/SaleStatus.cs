09e2f77f31232a3cf341af1197282a910129aac1
Jones Andrei Mensch <jones.mensch@outlook.com>
2022-11-22 15:59:24 -0300

    Criadas outras entidades e segunda Migration

---
+++ WebSalesMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSalesMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending, 
+        Billed,
+        Canceled
+    }
+}
