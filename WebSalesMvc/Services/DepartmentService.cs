06ab04315610b633282d29e44eb75e97330e4457
Jones Andrei Mensch <jones.mensch@outlook.com>
2022-12-01 15:20:53 -0300

    SellerFormViewModel e DepartmentService

---
+++ WebSalesMvc/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebSalesMvc.Data;
+using WebSalesMvc.Models;
+
+namespace WebSalesMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly WebSalesMvcContext _context;
+
+        public DepartmentService(WebSalesMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
+
--- WebSalesMvc/Services/SellerService.cs
+++ WebSalesMvc/Services/SellerService.cs
@@ -20,7 +20,6 @@ namespace WebSalesMvc.Services
         }
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
